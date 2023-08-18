aeb876cbabedffce0709425c0ce8462c66b0aa5a
Felipe Mendes <felipemadruga@gmail.com>
2023-08-18 19:24:03 -0300

    Conclui requisito 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,19 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    if not word:
+        return False
+
+    if len(word) <= 1:
+        return True
+
+    if low_index > high_index:
+        return True
+
+    if word[low_index] != word[high_index]:
+        return False
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
     raise NotImplementedError
+
+
+# print(is_palindrome_recursive("gg", 0, 1))
