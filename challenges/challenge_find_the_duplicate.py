6ea48f9f49cadac7445bcb630acbe9c07612defa
Felipe Mendes <felipemadruga@gmail.com>
2023-08-21 12:07:16 -0300

    Conclui requisito 05
    Teste local ficou correndo por tempo indefinido, então resolvi enviar pra avaliador remoto

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,29 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    has_string = any(type(num) == str for num in nums)
+    if has_string:
+        return False
+
+    has_negative = any(num < 0 for num in nums)
+    is_empty = len(nums) == 0
+    has_single_num = len(nums) == 1
+
+    if has_negative or is_empty or has_single_num:
+        return False
+
+    #    nums_sorted = sorted(nums)
+    #     current_num = sorted[0]
+
+    num_organizer = []
+    for num in nums:
+        if num in num_organizer:
+            return num
+        num_organizer.append(num)
+
+    return False
+
+    # raise NotImplementedError
+
+
+# nums = [1, 3, 4, 3, -2]
+# nums = ['a', 'b']
+# print(find_duplicate(nums))
