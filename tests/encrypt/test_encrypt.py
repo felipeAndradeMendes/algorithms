bc343462bf64e664d480e865872021395dad74af
Felipe Mendes <felipemadruga@gmail.com>
2023-08-21 16:12:37 -0300

    Conclui requisito 02
    Talvez não tenha passado corretamente

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
 from challenges.challenge_encrypt_message import encrypt_message
-# import pytest
+import pytest
+
+
+def test_encrypt_message():
+    assert encrypt_message("amaral", -1) == "larama"
+
+    assert encrypt_message("amaral", 3) == "ama_lar"
+
+    assert encrypt_message("amaral", 4) == "la_rama"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(12, "key")
+
+    # with pytest.raises(TypeError, match="tipo inválido para message"):
+    #     encrypt_message([], 3)
 
 
 # def test_encrypt_message_incorrect_message_type():
@@ -12,13 +26,13 @@ from challenges.challenge_encrypt_message import encrypt_message
 #         encrypt_message("message", "key")
 
 
-def test_encrypt_message_negative_key():
-    assert encrypt_message('amaral', -1) == 'larama'
+# def test_encrypt_message_negative_key():
+#     assert encrypt_message('amaral', -1) == 'larama'
 
 
-def test_encrypt_message_key_odd():
-    assert encrypt_message("amaral", 3) == "ama_lar"
+# def test_encrypt_message_key_odd():
+#     assert encrypt_message("amaral", 3) == "ama_lar"
 
 
-def test_encrypt_message_key_even():
-    assert encrypt_message("amaral", 4) == "la_rama"
+# def test_encrypt_message_key_even():
+#     assert encrypt_message("amaral", 4) == "la_rama"
